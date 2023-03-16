f461ce204d0644317f29a34b4297ad825440be9a
Stephen Celis <stephen@stephencelis.com>
2023-03-16 12:01:26 -0700

    wip

--- Sources/Clocks/Internal/Yield.swift
+++ Sources/Clocks/Internal/Yield.swift
@@ -4,14 +4,17 @@ extension Task where Success == Failure, Failure == Never {
   // NB: We would love if this was not necessary, but due to a lack of async testing tools in Swift
   //     we're not sure if there is an alternative. See this forum post for more information:
   //     https://forums.swift.org/t/reliably-testing-code-that-adopts-swift-concurrency/57304
-  static func megaYield(
-    count: Int = max(
-      ProcessInfo.processInfo.environment["TASK_MEGA_YIELD_COUNT"].flatMap(Int.init) ?? 20,
-      20
-    )
-  ) async {
+  static func megaYield(count: Int = defaultMegaYieldCount) async {
     for _ in 1...count {
       await Task<Void, Never>.detached(priority: .background) { await Task.yield() }.value
     }
   }
 }
+
+let defaultMegaYieldCount = max(
+  1,
+  min(
+    ProcessInfo.processInfo.environment["TASK_MEGA_YIELD_COUNT"].flatMap(Int.init) ?? 20,
+    10_000
+  )
+)
