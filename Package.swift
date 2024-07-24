b038e21203dbe1678fab900e64db922eeeee06fa
Brandon Williams <mbrandonw@hey.com>
2024-07-24 13:44:20 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
   ],
   targets: [
     .target(
